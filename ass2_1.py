cf9a51cd5e55667ff809c7a1d0ed8d8b89d08eb4
balavikas113 <balavikas07@gmail.com>
2025-04-16 18:24:44 +0530

    Add files via upload

---
+++ ass2_1.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number:"))
+if(num/2==0):
+    print(num,"is a EVEN number")
+else:
+    print(num,"is a ODD number")
---
+++ ass2_2.py
@@ -0,0 +1,7 @@
+i=1
+sum=0
+for i in range(0,50):
+    i=i+1
+    sum=i+sum
+
+print("The sum of numbers from 1 to 50 is:",sum)
